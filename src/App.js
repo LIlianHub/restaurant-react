192a36331b1c8857f14290734866971d91352f1e
LILianHUb <lilian.ballejos@hotmail.fr>
2023-01-09 09:30:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  // La balise vide <> permet de s'assurer que les parenthèses contiennent une seule racine dans l'arbre HTML
+  return (
+    <>
+      <h1>Restauration Rapide </h1>
+      <p>Le meilleur endroit où manger sur les Cézeaux!</p>
+    </>
+  );
+}
+
+export default App;
